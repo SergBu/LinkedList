e2fe8277ba6160b28ca822f2c926e20a8ddc67bf
Sergei <sebudnikov@gmail.com>
2023-10-16 08:30:51 +0300

    Добавьте файлы проекта.

---
+++ LinkedList/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    //класс узла обобщённый, т.е. хранит данные любого типа
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }       
+        public T Data { get; init; }
+        public Node<T>? Next { get; set; }
+}
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> linkedList = new LinkedList<string>
+            {
+                // добавление элементов
+                "Tom",
+                "Alice",
+                "Bob",
+                "Sam"
+            };
+
+            // выводим элементы
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+
+            // удаляем элемент
+            linkedList.Remove("Alice");
+            Console.WriteLine("\nПосле удаления Alice");
+            foreach (var item in linkedList) Console.WriteLine(item);
+
+            // проверяем наличие элемента
+            bool isPresent = linkedList.Contains("Sam");
+            Console.WriteLine(isPresent ? "Sam присутствует" : "Sam отсутствует");
+
+            // добавляем элемент в начало            
+            linkedList.AppendFirst("Bill");
+            Console.WriteLine("\nПосле добавления Billa");
+            foreach (var item in linkedList) Console.WriteLine(item);
+        }
+    }
+}
